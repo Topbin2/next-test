d82c636ccc457d3c66bb10577abeb053f0fd4fdf
Topbin2 <mosangbin94@gmail.com>
2022-11-24 16:04:00 +0900

    test

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://boramyy.github.io/invitation"
+      : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
